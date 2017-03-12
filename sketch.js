cdaf31463c4d22c6ceab034f50f04264ddeddd82
Susie Gomez <sugo4237@colorado.edu>
2017-03-12 14:45:25 -0600

    setting up the grid

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var dots = [];
+var cols = 5;
+var rows = 5;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	// spacing between our grid
+	var spacing = windowWidth / cols; // space between dots in grid
+
+	// creating grid - setting x and y up to 5 cols & rows
+	for (var y = 0; y < rows; y ++) { // for every column
+		for (var x = 0; x < cols; x++) { // fill the entire row first
+
+			ySpace =  spacing + y * spacing; 
+			xSpace = x * spacing; 
+			dots.push(new Dot(xSpace, y * spacing, 2));
+		}
+	}
+
+	console.log(dots);
+}
+
+function draw() {
+	background('white');
+
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].display();
+	}
+}
+
+function Dot(x, y, r) {
+	this.x = x; 
+	this.y = y; 
+	this.r = r;
+
+	this.display = function() {
+		fill('green');
+		noStroke();
+		translate(this.x, this.y); 
+		ellipse(0, 0, this.r * 2); 
+	}
+}
+
+// resources: coding train tutorial on plinko challenge
